556c85ca301f679fe14008de40f9afe42331535c
Daniuu <Daniuu.Wikipedia@gmail.com>
2021-08-19 22:41:35 +0200

    Completing API query
    Code used to run the API queries.

--- AbuseFilter.py
+++ AbuseFilter.py
@@ -9,8 +9,15 @@ This code contains all the stuff required to process the various aspects of work
 
 from Interaction import Bot, NlBot, MetaHandler
 
+class AllFoundError(Exception):
+    "This error can be used to indicate that the bot could not find any new requests to handle"
+    
+
 class AbuseFilter:
     "This class provides the main functions for the Abuse filters (get hits and process them)"
+    
+    meta = MetaHandler()
+    
     def __init__(self, bot, number):
         assert isinstance(bot, Bot), "Please provide a valid bot object"
         self.bot = bot #Provide an instance of the Bot class
@@ -19,6 +26,14 @@ class AbuseFilter:
     
     def __str__(self):
         return self.bot.api + ' ' + str(self.filter)
+    
+    def get_hits(self, cont=None, start=None):
+        "Get the first 50 hits, starting from cont (for continuation of previous requests)"
+        dic = {'action':'query',
+               'list':'abuselog',
+               'aflfilter':self.filter,
+               'aflprop':'user|title|timestamp|details'}
+        
     
     
     
